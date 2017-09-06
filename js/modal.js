07f42a84cb97c8514a10ee77c53ce85a064bd867
Ilya <bc_ilya@mail.ru>
2017-09-06 21:10:09 +0300

    js и исправление багов
    - Модальные окна работают, а не болтаются за футером;
    - Скролл вправо починен;
    - Мелкие поправки семантики;
    - И ещё мелкие поправки.

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,27 @@
 var writeUs = document.querySelector(".write-us-btn");
+var writeUsPopup = document.querySelector(".modal-write-us");
+var writeUsClose = document.querySelector(".modal-write-us .modal-close");
 
-console.log(writeUs);
+var	map = document.querySelector(".map-btn");
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = document.querySelector(".modal-map .modal-close");
+
+writeUs.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	writeUsPopup.classList.add("modal-show");
+});
+
+writeUsClose.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	writeUsPopup.classList.remove("modal-show");
+})
+
+map.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	mapPopup.classList.add("modal-show");
+})
+
+mapClose.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	mapPopup.classList.remove("modal-show");
+})
